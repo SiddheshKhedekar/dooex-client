c75d9ca7b4ed6a2ea897ca2b74458f195aa9934e
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-10-13 23:06:43 +0530

    add hot reloading

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from 'App';
+import { AppContainer } from 'react-hot-loader';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function render(Component) {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+render(App);
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const nextApp = require('./App').default;
+
+    render(nextApp);
+  });
+}
