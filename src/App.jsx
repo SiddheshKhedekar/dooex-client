d8c684f56e51e38328ba6ba0897865fb195811b7
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-10-14 22:40:51 +0530

    remove unnecessary logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import TopNav from 'components/TopNav';
 
 import styles from './App.scss';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return [<TopNav />, <Home />];
