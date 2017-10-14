471256647277cf9a38b6980c800bb85e2115c8f4
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-10-14 21:16:58 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,26 @@ import Doodle from './Doodle';
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
-    return (
+    return [
+      <nav className={styles.nav}>
+        <a className="navbar-brand" href="#">
+          Fixed
+        </a>
+      </nav>,
+
       <div className="container-fluid">
         <div className="row justify-content-center">
           <div id="row" className={styles.row}>
-            <Doodle />
-            <Doodle />
-            <Doodle />
-            <Doodle />
+            <div className="col-12">
+              <Doodle />
+              <Doodle />
+              <Doodle />
+              <Doodle />
+            </div>
           </div>
         </div>
-      </div>
-    );
+      </div>,
+    ];
   }
 }
 
