f48db16dfd96e8118f177a57a892457c93df1e4a
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-10-17 09:30:54 +0530

    add native share button for sharing app

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -4,12 +4,45 @@ import React from 'react';
 
 import styles from './TopNav.scss';
 
+function shareApp() {
+  navigator.share({
+    title: 'DooEx',
+    text: 'DooEx\nGoogle Doodles Explorer\n\n',
+    url: 'https://10.42.0.1:3000/',
+  });
+}
+
+function ShareButton() {
+  if (navigator.share === undefined) {
+    return null;
+  }
+
+  return (
+    <button className={styles.action} onClick={shareApp}>
+      <span className="fa fa-fw fa-share-alt" />
+    </button>
+  );
+}
+
 function TopNav() {
   return (
     <nav className={styles.root}>
       <a className="navbar-brand" href="/">
         Fixed
       </a>
+
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <button className={styles.action}>
+            <span className="fa fa-fw fa-search" />
+          </button>
+        </li>
+
+        {/* last item */}
+        <li className="nav-item">
+          <ShareButton />
+        </li>
+      </ul>
     </nav>
   );
 }
