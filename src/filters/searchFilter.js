de4454e7ef7cf6e41d39960641f91982ec58d145
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-10-22 00:09:40 +0530

    add module to regex-escape strings

--- src/filters/searchFilter.js
+++ src/filters/searchFilter.js
@@ -2,12 +2,14 @@
 
 import type { Doodle } from 'modules/types';
 
+import escape from 'modules/regexp-escape';
+
 function searchFilter(doodles: Array<Doodle>, keyword: string = ''): Array<Doodle> {
   if (keyword === '') {
     return [];
   }
 
-  const filterRegexp = new RegExp(`\\b${keyword}\\b`, 'i');
+  const filterRegexp = new RegExp(escape(keyword), 'i');
   const matchingDoodles = doodles.filter(doodle => filterRegexp.test(doodle.title));
 
   return matchingDoodles;
