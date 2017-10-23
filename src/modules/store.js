2dc4694c0ef9882863e261b5d84cb3344b478c5f
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-10-23 18:09:59 +0530

    sync location to reducers

--- src/modules/store.js
+++ src/modules/store.js
@@ -4,7 +4,8 @@ import { applyMiddleware, createStore } from 'redux';
 
 import rootReducer from 'reducers/root-reducer';
 import persistSavedDoodles from 'modules/persist-saved-doodles';
+import syncLocation from 'modules/sync-location';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, persistSavedDoodles));
+const store = createStore(rootReducer, applyMiddleware(thunk, persistSavedDoodles, syncLocation));
 
 export default store;
