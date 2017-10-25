e7a5e366b44e3651b38283c4f6276ba2692dbae1
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-10-26 04:11:15 +0530

    add webpack-bundle-analyzer

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,6 +12,8 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
 
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
 const publicPath = paths.servedPath;
@@ -299,6 +301,10 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+    }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
