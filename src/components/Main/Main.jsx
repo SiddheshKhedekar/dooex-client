e7789c4cebfb10c000cc8d18fd335d43e1e86de6
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-10-26 23:28:17 +0530

    remove buggy shouldComponentUpdate() from <Main />

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -38,14 +38,6 @@ class Main extends Component<Props> {
     window.addEventListener('scroll', this.handleScroll);
   }
 
-  shouldComponentUpdate(nextProps: Props) {
-    if (nextProps.doodles.length === this.props.doodles.length) {
-      return false;
-    }
-
-    return true;
-  }
-
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
   }
