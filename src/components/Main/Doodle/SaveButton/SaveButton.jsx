214113a8869030dc67d980f74812810a3d82e490
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-11-06 14:15:11 +0530

    redefine <SaveButton />

--- src/components/Main/Doodle/SaveButton/SaveButton.jsx
+++ src/components/Main/Doodle/SaveButton/SaveButton.jsx
@@ -7,10 +7,45 @@ import { connect } from 'react-redux';
 
 import Alert from 'components/Alert';
 
-import { cacheDoodle, uncacheDoodle } from 'modules/cache-doodles';
+import { savedDoodlesCacheName as CACHE_NAME } from 'modules/names';
 import { updateDoodle } from 'reducers/doodles';
 
 import styles from './SaveButton.css';
+
+/**
+ * Filter out null links in doodle.
+ */
+function getValidLinks(doodle: Doodle) {
+  const linkTypes = ['url', 'hires_url'];
+
+  return linkTypes.map(linkType => doodle[linkType]).filter(link => link !== null);
+}
+
+/**
+ * Add doodle resources to cache.
+ */
+async function cacheDoodle(doodle: Doodle) {
+  const cache = await caches.open(CACHE_NAME);
+
+  const cachePromises = getValidLinks(doodle).map(async (link) => {
+    const resp = await fetch(link, { mode: 'no-cors' });
+
+    return cache.put(link, resp);
+  });
+
+  return Promise.all(cachePromises);
+}
+
+/**
+ * Remove doodle resources from cache.
+ */
+async function uncacheDoodle(doodle: Doodle) {
+  const cache = await caches.open(CACHE_NAME);
+
+  const cachePromises = getValidLinks(doodle).map(async link => cache.delete(link));
+
+  return Promise.all(cachePromises);
+}
 
 type Props = {
   doodle: Doodle,
