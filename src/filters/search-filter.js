e648b134a46f4ce9d7b8dac0936ce8d9f816b7ff
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-11-06 14:18:16 +0530

    redefine search filter

--- src/filters/search-filter.js
+++ src/filters/search-filter.js
@@ -2,7 +2,28 @@
 
 import type { Doodle } from 'modules/types';
 
-import escape from 'modules/regexp-escape';
+/**
+ * Escape string for use in regular expression.
+ */
+function escape(pattern) {
+  const alphanum = '_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890';
+
+  const s = pattern.split('');
+
+  for (let i = 0; i < pattern.length; i += 1) {
+    const c = pattern[i];
+
+    if (!alphanum.includes(c)) {
+      if (c == '\u0000') {
+        s[i] = '\\000';
+      } else {
+        s[i] = `\\${c}`;
+      }
+    }
+  }
+
+  return s.join('');
+}
 
 function searchFilter(doodles: Array<Doodle>, keyword: string = ''): Array<Doodle> {
   if (keyword === '') {
